019246e193e7778e1f731147cf9d337a0f0ddcd3
oktavianu <oktavianu@kali.home.net>
2024-08-17 05:44:03 +0700

    new binary search file

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,24 @@
 def bin_search(arr, target_value):
-    minn = 0
-    maxx = len(arr) - 1
+    minn = 0 # min is 0 since the counting starts from 0
+    maxx = len(arr) - 1 # the max is set to length of the array since 
     gss = 0
-
-    # condition checking
-    while gss != target_value:
-        index_target = arr.index(target_value)
-        target_value = arr[index_target]
-        
+    
+    # condition checking: this loop while run as long as maxx > minn
+    while maxx > minn:
         # first guess is the average of minn and maxx
         gss = arr[(minn + maxx) // 2]
 
-        if gss == target_value:
-            return arr.index(target_value)
-        elif gss < target_value:
+        if arr.index(gss) == arr.index(target_value):
+            return arr.index(gss)
+        elif arr.index(gss) < arr.index(target_value):  
             minn = arr.index(gss) + 1
-        elif gss > target_value:
+        elif arr.index(gss) > arr.index(target_value):
             maxx = arr.index(gss) - 1
     
+    # -1 will be returned if target value is not in the list 
     return -1
 
-data = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 target = int(input("Enter a prime number: "))
-result = bin_search(data, target)
+result = bin_search(primes, target)
 print("Data point found at index ", result)
